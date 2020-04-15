49bd6da3ae6db1758228f1a3237abf7790c673fe
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-04-15 17:20:53 -0600

    Add passing cats

--- initialize/test/cat_test.rb
+++ initialize/test/cat_test.rb
@@ -9,11 +9,18 @@ class CatTest < Minitest::Test
   end
 
   def test_it_has_a_name
+    # skip
     carlos = Cat.new("Carlos")
     assert_equal "Carlos", carlos.name
   end
 
+  def test_it_can_have_a_different_name
+    katie = Cat.new("Katie")
+    assert_equal "Katie", katie.name
+  end
+
   def test_it_has_a_sound
+    # skip
     carlos = Cat.new("Carlos")
     assert_equal "meow", carlos.sound
   end
